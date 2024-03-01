595468ecc6d57b29b52ef122a823a1484e632c13
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-03-01 23:19:48 +0800

    add authentication to blade template

--- twitter-clone/resources/views/shared/submit-idea.blade.php
+++ twitter-clone/resources/views/shared/submit-idea.blade.php
@@ -1,15 +1,20 @@
-<h4> Share yours ideas </h4>
-<form action="{{ route('ideas.store') }}" method="post">
-    @csrf
-    <div class="row">
-        <div class="mb-3">
-            <textarea name="content" class="form-control" id="idea" rows="3"></textarea>
-            @error('content')
-                <span class="fs-6 text-danger">{{ $message }}</span>
-            @enderror
+@auth
+    <h4> Share yours ideas </h4>
+    <form action="{{ route('ideas.store') }}" method="post">
+        @csrf
+        <div class="row">
+            <div class="mb-3">
+                <textarea name="content" class="form-control" id="idea" rows="3"></textarea>
+                @error('content')
+                    <span class="fs-6 text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="">
+                <button class="btn btn-dark"> Share </button>
+            </div>
         </div>
-        <div class="">
-            <button class="btn btn-dark"> Share </button>
-        </div>
-    </div>
-</form>
+    </form>
+@endauth()
+@guest
+    <h1>Login to share idea</h1>
+@endguest
