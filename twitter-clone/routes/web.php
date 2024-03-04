3b453a470aa4d0c5ac75d0e5030680882e29efde
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-03-04 23:07:37 +0800

    add route group

--- twitter-clone/routes/web.php
+++ twitter-clone/routes/web.php
@@ -19,17 +19,22 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['prefix'=>'ideas/', 'as'=>'ideas.', 'middleware' => ['auth']],function(){
 
-    Route::post('', [IdeaController::class, 'store'])->name('store')->withoutMiddleware(['auth']);
+    Route::post('', [IdeaController::class, 'store'])->name('store');
 
-    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware(['auth']);
+    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
 
-    Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+    Route::group(['middleware' => ['auth']], function(){
 
-    Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
 
-    Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
+        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
 
-    Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+        Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
+
+        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+    });
+
+ 
 
 });
 
@@ -37,15 +42,7 @@ Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/profile', [ProfileController::class, 'profile']);
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
 
-Route::post('/register', [AuthController::class, 'store']);
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::get('/terms', function(){
