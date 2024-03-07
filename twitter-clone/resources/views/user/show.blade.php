8ce1cabfde2e52988dbb7a1807a654b21691ab04
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-03-07 16:22:21 +0800

    feat: profile page

---
+++ twitter-clone/resources/views/user/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.layout')
+@section('content-section')
+    <div class="row">
+        {{-- <div class="d-flex">
+            @foreach ($users as $user)
+                <p>{{$user['name']}}</p><p>{{$user['age']}}</p>
+            @endforeach
+            </div>
+         --}}
+
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            @include('shared.success-message')
+            <hr>
+            <div class="mt-3">
+                @include('shared.user-card')
+            </div>
+            <hr>
+            @forelse ($ideas as $idea)
+                @include('shared.idea-card')
+            @empty
+                <p>No Results found!</p>
+            @endforelse
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
+@endsection
