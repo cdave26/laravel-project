1197ebae128a452b8ea8db08cf1c32d956404d4b
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-03-08 17:58:26 +0800

    feat: add mailer after registering user

--- twitter-clone/app/Http/Controllers/AuthController.php
+++ twitter-clone/app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\WelcomeEmail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -29,6 +31,8 @@ class AuthController extends Controller
                 'password' => Hash::make($validated['password']),
             ]
          );
+
+         Mail::to($user->email)->send(new WelcomeEmail($user));
 
          return redirect()->route('register')->with('success', 'account created successfully');
     }
