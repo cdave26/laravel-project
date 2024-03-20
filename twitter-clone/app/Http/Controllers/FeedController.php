49dcfdc9447b218341399f806cfb530a570f00ef
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-03-20 21:42:20 +0800

    feat: add feed invokable controller

---
+++ twitter-clone/app/Http/Controllers/FeedController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class FeedController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $ideas =  Idea::orderBy('created_at','DESC');
+
+        if(request()->has('search')){
+            $ideas = $ideas->where('content','like', '%' . request()->get('search','') . '%');
+        }
+        #query all idea tabl
+        //select all from idea
+
+
+        return view('dashboard',[
+            'ideas' => $ideas->paginate(5)
+        ]);
+    }
+}
