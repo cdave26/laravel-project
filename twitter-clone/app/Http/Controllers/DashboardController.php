741156815dab706519bfe67e12bbbbe7363c89f4
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-07-23 14:16:47 +0800

    feat: add global blade variable

--- twitter-clone/app/Http/Controllers/DashboardController.php
+++ twitter-clone/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
--- twitter-clone/app/Providers/AppServiceProvider.php
+++ twitter-clone/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        // get user who has a top ideas posted
+        View::share('topUsers', 
+            User::withCount('ideas')
+            ->orderBy('ideas_count', 'DESC')
+            ->limit(5)
+            ->get());
     }
 }
