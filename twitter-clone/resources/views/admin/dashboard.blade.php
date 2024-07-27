e1b2db72ad08844151e80177d5e7805695d63f6d
DESKTOP-K5H9KBN\Dave <ctampoy26@gmail.com>
2024-07-27 10:47:26 +0800

    feat: add widget on admin dashboard

--- twitter-clone/resources/views/admin/dashboard.blade.php
+++ twitter-clone/resources/views/admin/dashboard.blade.php
@@ -7,6 +7,29 @@
         </div>
         <div class="col-9">
             <h1>Admin Dashboard</h1>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    @include("shared.widgets", [
+                        "title" => "Total Users",
+                        "icon" => "fa fa-users",
+                        "data" => $totalUsers,
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include("shared.widgets", [
+                        "title" => "Total Ideas",
+                        "icon" => "fa fa-lightbulb",
+                        "data" => $totalIdea,
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include("shared.widgets", [
+                        "title" => "Total Comments",
+                        "icon" => "fa fa-comment",
+                        "data" => $totalComments,
+                    ])
+                </div>
+            </div>
         </div>
     </div>
 @endsection
